70455b4d650c361b967322449b1417b4d55d241c
mohit <mp7195539@gmail.com>
2025-10-29 10:06:16 +0530

    app.js change

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -24,7 +24,7 @@ function App() {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [editingTransaction, setEditingTransaction] = useState(null);
 
-  const API_BASE_URL = 'http://localhost:5000/api/v1';
+ const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api/v1';
 
   // --- Initial Data Fetch ---
   const getAllData = async () => {
